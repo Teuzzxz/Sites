133759f6a9b79976c80f5d7899c96acdba2cd9ec
Teuzzxz <mateusramosmrpc@gmail.com>
2024-07-23 18:56:12 -0300

    ficando bom

--- Barreto/código/script.js
+++ Barreto/código/script.js
@@ -4,9 +4,12 @@ const linhas = [...document.getElementsByClassName('line')]
 const ani = [...document.getElementsByClassName('ani')]
 const Btn_Menu = document.getElementsByClassName('BotãoMenu')[0]
 const barreto_boneco = document.getElementById('barreto')
+const Serviços = document.getElementsByClassName('Serviços')[0]
+const conteudo = document.getElementsByClassName('all')[0]
 let travado = false
 let consultor = ''
 var trocar = true
+let ladoE = ''
 
 P_Menu.map((v, p) => {
     
@@ -48,7 +51,7 @@ P_Menu.map((v, p) => {
         ani[0].setAttribute('style' , `width:${P_Menu[0].offsetWidth}px;height:2px;background-Color:black;position:absolute;`)
     }
     if (!travado) {
-        incio()
+        setTimeout(incio,500)
         travado = true
     }
 })
@@ -63,3 +66,32 @@ Btn_Menu.addEventListener('click', () => {
     }
     
 })
+const incioBarreto = (tamX) => {
+    setTimeout(() => {
+        if (tamX > 950) {
+            barreto_boneco.setAttribute('style',`left:10px;opacity:100%;`)
+        }
+    })
+
+}
+incioBarreto(window.innerWidth)
+if (window.innerWidth < 950) {
+    ladoE = true
+} else if (window.innerWidth > 950) {
+    ladoE = false
+}
+window.addEventListener('resize', (evt) => {
+    if (evt.currentTarget.innerWidth < 949 && !ladoE) {
+        barreto_boneco.setAttribute('style', `display:none;`)
+        console.log('voltou');
+        ladoE = true
+    } else if (evt.currentTarget.innerWidth > 950 && ladoE ) {
+        barreto_boneco.setAttribute('style', `left:-490px;opacity:20%;display:block;`)
+        barreto_boneco.setAttribute('style', `left:10px;opacity:100%;display:block;`)
+        console.log('passou');
+        ladoE = false
+    }
+    console.log(ladoE);
+})
+
+
